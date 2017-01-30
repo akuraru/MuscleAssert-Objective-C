42e1452038ac324b31566a1a4487f279758b2ea6
akuraru <akuraru@gmail.com>
2017-01-30 22:10:09 +0900

    formatter外部から設定できるようにする

--- MuscleAssert/MuscleAssert.h
+++ MuscleAssert/MuscleAssert.h
@@ -7,6 +7,7 @@
 //
 
 #import "MACustomDiff.h"
+#import "MAFormatterProtocol.h"
 
 #define MAssert(_left,_right) XCTAssertNil([[[MuscleAssert alloc] init] deepStricEqual:_left right:_right])
 
@@ -15,6 +16,8 @@ NS_ASSUME_NONNULL_BEGIN
 @class MACustomDiff;
 
 @interface MuscleAssert : NSObject <MSDeepDiffProtocol>
+
+@property (nonatomic, nonnull) id<MAFormatterProtocol> formatter;
 
 - (NSString *_Nullable)deepStricEqual:(id _Nullable)left right:(id _Nullable)right;
 - (NSString *_Nullable)deepStricEqual:(id _Nullable)left right:(id _Nullable)right message:(NSString *_Nullable)message;
